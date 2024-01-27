9eb988bd5fdb7da5436dd9ad79b853534e086840
Sunil <sunil.kumar@snipp.com>
2024-01-27 20:31:31 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+//const product = 'Socks'
+
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'Green sock full size'
+        }
+    }
+})
