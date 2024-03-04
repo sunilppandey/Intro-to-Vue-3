83eebbc39706cd80bd2e410fa8a0b453fbbce42b
Sunil <sunil.kumar@snipp.com>
2024-03-04 13:04:42 +0530

    L5 List rendering end

--- main.js
+++ main.js
@@ -7,7 +7,13 @@ const app = Vue.createApp({
             image: './assets/images/socks_blue.jpg',
             inStock: true,
             inventory: 11,
-            onSale: true
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green'},
+                { id: 2235, color: 'blue'}
+            ],
+            sizes: [1,2,3,4,5]
         }
     }
 })
