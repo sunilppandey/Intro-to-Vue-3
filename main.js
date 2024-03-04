98e139006391095c0c61aabed8ebb630c610acb9
Sunil <sunil.kumar@snipp.com>
2024-03-04 19:54:52 +0530

    L9 Component & Prod lecture

--- main.js
+++ main.js
@@ -3,39 +3,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            selectedVariant: 0,
-            inventory: 11,
-            onSale: true,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0}
-            ],
-            cart:0
+            cart: 0,
+            premium: true
         }
     },
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        removeFromCart() {
-            this.cart -=1 
-        },
-        updateVariant(index) {
-            this.selectedVariant = index
-        }
-    },
-    computed: {
-        title() {
-            return this.brand + " " + this.product
-        },
-        image() {
-            return this.variants[this.selectedVariant].image
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity
-        }
-    }
+    methods: {}
 })
